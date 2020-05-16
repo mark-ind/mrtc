239af10a2d01c973a617bb21d79174d5a8383270
Marco Medrano <marco.medrano.garay@gmail.com>
2020-05-16 00:26:47 -0400

    support for audio on windows and setup size based on display

--- src/screenCapture/ElectronScreenShare.ts
+++ src/screenCapture/ElectronScreenShare.ts
@@ -2,40 +2,61 @@ import Logger from 'src/log/Logger';
 
 export default class ElectronScreenShare {
   private static logger = Logger.getLogger(ElectronScreenShare.name);
-  
+
   public static async share(options: {}): Promise<MediaStream> {
     this.logger.info(`Options`, options);
 
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { desktopCapturer } = require('electron');
-    const sources = await desktopCapturer.getSources({ types: ['screen'] });
-    const source = sources[0];
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { desktopCapturer, remote } = require('electron');
 
-    this.logger.info(`Screen source to share`, source);
-    
-    const defaultOptions = {
-      video: {
-        // cursor: "always",
-        mandatory: {
-          chromeMediaSource: 'desktop',
-          chromeMediaSourceId: source.id,
-          minWidth: 1280,
-          maxWidth: 1280,
-          minHeight: 720,
-          maxHeight: 720
+      this.logger.info(`Displays`, remote.screen.getPrimaryDisplay());
+      const sources = await desktopCapturer.getSources({ types: ['screen'] });
+      const primaryDisplay = remote.screen.getPrimaryDisplay();
+      const source = sources.find((s: any) => s.id.includes(primaryDisplay.id));
+
+      this.logger.info(`Screen source to share`, source);
+
+      const defaultConstrains = {
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: source.id,
+            minWidth: primaryDisplay.size.width,
+            minHeight: primaryDisplay.size.height,
+            maxFrameRate: 25
+          }
+        },
+      }
+
+
+      const audioConstrains = {
+        audio: {
+          mandatory: {
+            chromeMediaSource: 'desktop'
+          }
         }
-      },
-      audio: false,
-      // audio: {
-      //   echoCancellation: true,
-      //   noiseSuppression: true,
-      //   sampleRate: 44100
-      // }
+      }
+
+      const finalConstrains = {};
+      const isWin = process.platform === "win32";
+
+      // audio does not work in mac https://www.electronjs.org/docs/api/desktop-capturer
+      if (isWin)
+        Object.assign(finalConstrains, defaultConstrains, audioConstrains);
+      else
+        Object.assign(finalConstrains, defaultConstrains);
+
+
+      this.logger.debug('Requesting screen access with', finalConstrains);
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      return await navigator.mediaDevices.getUserMedia(finalConstrains);
+    } catch (e) {
+      const errorMessage = `Failed to screen share`;
+      this.logger.error(errorMessage, e);
+      throw e;
     }
-
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    return await navigator.mediaDevices.getUserMedia(defaultOptions);
   }
 }
